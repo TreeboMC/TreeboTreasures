fb68304a0f5e2c22f72b63f96b95bd8344c6c4a8
ShakeforProtein <github@taktimer.net>
2019-05-08 01:46:54 +1000

    Initial commits Project Base

--- src/main/java/me/shakeforprotein/treebotreasures/Guis/RewardsGui.java
+++ src/main/java/me/shakeforprotein/treebotreasures/Guis/RewardsGui.java
@@ -25,25 +25,38 @@ public class RewardsGui {
     public void rewardsGui(Player p) {
 
         int rows = pl.getConfig().getInt("gui.rows");
-        String title = ChatColor.translateAlternateColorCodes('&', pl.getConfig().getString("gui.title").replace("{badge}", pl.badge));
+        String title = ChatColor.translateAlternateColorCodes('&', pl.getConfig().getString("gui.title") + "");
 
         Inventory thisInv = Bukkit.createInventory(null, rows * 9, title);
-
+        for (int slot = 0; slot < thisInv.getSize(); slot++) {
+            ItemStack fillerItem = new ItemStack(Material.valueOf(pl.getConfig().getString("gui.filler")));
+            ItemMeta fillMeta = fillerItem.getItemMeta();
+            fillMeta.setDisplayName("");
+            fillerItem.setItemMeta(fillMeta);
+            thisInv.setItem(slot, fillerItem);
+        }
         for (String menuItem : pl.getConfig().getConfigurationSection("gui.items").getKeys(false)) {
+            int position = pl.getConfig().getInt("gui.items." + menuItem + ".position");
             ItemStack newItem = new ItemStack(Material.valueOf(pl.getConfig().getString("gui.items." + menuItem + ".material")), 1);
             ItemMeta newMeta = newItem.getItemMeta();
             List<String> newLore = new ArrayList<>();
             int keys = 0;
-            if(pl.getConfig().get("keys." + p.getUniqueId() + "." + menuItem.toUpperCase()) != null){
+            if (pl.getConfig().get("keys." + p.getUniqueId() + "." + menuItem.toUpperCase()) != null) {
                 keys = pl.getConfig().getInt("keys." + p.getUniqueId() + "." + menuItem.toUpperCase());
             }
-            newLore.add("You have -* " + ChatColor.translateAlternateColorCodes('&',"&6" + keys) + " *- &r" + menuItem + " keys");
+            newLore.add(ChatColor.translateAlternateColorCodes('&', pl.getConfig().getString("gui.countLore").replace("{keyCount}", keys + "").replace("{keyType}", menuItem)));
             newMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', pl.getConfig().getString("gui.items." + menuItem + ".title")));
             newMeta.setLore(newLore);
-            int position = pl.getConfig().getInt("gui.items." + menuItem + ".position");
             newItem.setItemMeta(newMeta);
             thisInv.setItem(position, newItem);
         }
+        for (int slot = 0; slot < thisInv.getSize(); slot++) {
+            if (thisInv.getItem(slot).getType() == Material.AIR) {
+                ItemStack airItem = new ItemStack(Material.AIR);
+                thisInv.setItem(slot, airItem);
+            }
+        }
+
         p.openInventory(thisInv);
     }
 
